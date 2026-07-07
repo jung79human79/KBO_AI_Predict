4b447aeadf7ab7ff14f8801512bda985d38309a3
jdj <wjd1eo2wn3@naver.com>
2026-07-07 18:12:06 +0900

    Connect database with environment variables

--- database/db.py
+++ database/db.py
@@ -2,16 +2,19 @@ import os
 import pymysql
 
 db_config = {
-    "host": os.getenv("DB_HOST"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+    "port": int(os.environ.get("DB_PORT", 3306)),
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASSWORD"),
+    "database": os.environ.get("DB_NAME"),
     "charset": "utf8mb4"
 }
+
 
 # Pandas 전용
 def get_db_connection():
     return pymysql.connect(**db_config)
+
 
 # Cursor 전용
 def get_cursor_connection():
